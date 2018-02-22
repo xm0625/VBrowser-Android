363d7a7e33fa454e9e4443e7897080efe8724643
zhengxian.lzx@alibaba-inc.com <zhengxian.lzx@alibaba-inc.com>
2018-02-22 15:10:06 +0800

    修复m3u8文件对应的mimetype返回null的问题,会导致m3u8的url无法被播放器识别

--- app/src/main/java/com/xm/vbrowser/app/util/IntentUtil.java
+++ app/src/main/java/com/xm/vbrowser/app/util/IntentUtil.java
@@ -18,6 +18,12 @@ public class IntentUtil {
         if (extension != null) {
             //使用系统API，获取MimeTypeMap的单例实例，然后调用其内部方法获取文件后缀名（扩展名）所对应的MIME类型
             type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            if (type == null){
+                if ("m3u8".equals(extension)){
+                    //m3u8只是Unicode版本的m3u而已,目前(Android O)MimeTypeMap里没有m3u8对应的mimetype
+                    type = MimeTypeMap.getSingleton().getMimeTypeFromExtension("m3u");
+                }
+            }
         }
         Log.i("bqt", "系统定义的MIME类型为：" + type);
         return type;
